6ede70fb32bfee30a7c6ea598e0a39cd7164f15d
Humberto Lacerda <pr.humberto.lacerda@gmail.com>
2024-10-31 18:19:11 -0300

    Conclusão do desafio de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+Console.WriteLine("SmartPhone Nokia:");
+Smartphone nokia = new Nokia("27991921563", "SK21A", "101222251", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatSapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone iPhone:");
+Smartphone iphone = new Iphone("27991921563", "STJ21KL", "1012222978", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
